9883c7ade4f98ee653a638145dbb659b0d70d01a
Punit Kumar <mr.punitkr@gmail.com>
2023-07-04 12:46:08 +0530

    intial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    // Redirect to a specified URL
+    const redirectTo = (url) => {
+      window.location.href = url;
+    };
+
+    redirectTo('https://salemseats.com');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Redirecting...</h1>
+      {/* Add any additional content or components here */}
     </div>
   );
 }
